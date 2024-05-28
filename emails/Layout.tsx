03c87b5c6ee34e19616a713ebb35b8ccc53ac18a
庞晓峰 <335136854@qq.com>
2024-05-28 15:25:07 +0800

    fix: 更新配置

--- emails/Layout.tsx
+++ emails/Layout.tsx
@@ -39,7 +39,7 @@ export default function Layout({
                 src={`${emailConfig.baseUrl}/icon.png`}
                 width="24"
                 height="24"
-                alt="Cali"
+                alt="pxfe"
                 className="mx-auto my-0"
               />
               <Text className="text-center">
@@ -47,7 +47,7 @@ export default function Layout({
                   href="https://pxfe.top"
                   className="text-zinc-700 underline"
                 >
-                  <strong>Cali Castle</strong>
+                  <strong>PXFE SITE</strong>
                 </Link>
                 <br />
                 开发者
--- emails/NewReplyComment.tsx
+++ emails/NewReplyComment.tsx
@@ -6,12 +6,12 @@ import { Button, Heading, Hr, Img, Link, Section, Text } from './_components'
 import Layout from './Layout'
 
 const NewReplyCommentEmail = ({
-  postLink = 'https://cali.so',
+  postLink = 'https://pxfe.top',
   postTitle = '测试文章 Lorem ipsum dolor sit amet, consectetur adipisicing elit',
   postImageUrl = 'https://cn.zolplay.com/api/og?title=测试文章',
-  userFirstName = 'Cali',
-  userLastName = 'Castle',
-  userImageUrl = 'https://cali.so/icon.png',
+  userFirstName = 'PXFE',
+  userLastName = 'SITE',
+  userImageUrl = 'https://pxfe.top/icon.png',
   commentContent = '*测试评论*\n- Wow wtf\n- Cool',
 }: {
   postLink?: string | null
