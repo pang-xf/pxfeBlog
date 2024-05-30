10eb3ae35dfa523e5222339ae4e73e97d2710bb1
liyu <me0809@yeah.net>
2024-05-30 22:26:16 +0800

    fix

--- config/email.ts
+++ config/email.ts
@@ -2,8 +2,9 @@ import { env } from '~/env.mjs'
 
 export const emailConfig = {
   from: env.NEXT_PUBLIC_SITE_EMAIL_FROM,
-  baseUrl:
-    env.NODE_ENV === 'production'
-      ? env.NEXT_PUBLIC_SITE_URL
-      : 'http://localhost:3000',
+  // baseUrl:
+  //   env.NODE_ENV === 'production'
+  //     ? env.NEXT_PUBLIC_SITE_URL
+  //     : 'http://localhost:3000',
+  baseUrl: env.NEXT_PUBLIC_SITE_URL
 }
